16da986cd5295b6f95f677ff2f15e5b4444f38bb
EdoardoEula <115698580+EdoardoEula@users.noreply.github.com>
2022-10-25 12:30:48 +0200

    hypnogram

--- EEG_sleep.m
+++ EEG_sleep.m
@@ -6,7 +6,7 @@ load("record.mat");
 EEG = double(record);
 clear record
 fs_EEG = 512;
-EEG = EEG(2*60*fs_EEG:end);
+EEG = EEG(5*60*fs_EEG:end);
 EEG = EEG((length(EEG)/fs_EEG - 5*60)*fs_EEG:-1:1);
 N = length(EEG);
 %% Filtering
@@ -132,15 +132,105 @@ legend('alpha', 'theta', 'delta')
 
 %% 
 st = 1:5;
-domTab = cell(floor(length(EEG)/ep_samples),1);
+domTab = cell(floor(length(EEG)/ep_samples),3);
 for i = 1:floor(length(EEG)/ep_samples)
     j = perc{i,:} == max(perc{i,:});
-    domTab{i} = perc.Properties.VariableNames{j};
+    h = perc{i,:} == min(perc{i,:});
+    domTab{i,2} = perc.Properties.VariableNames{j};
+    domTab{i,1} = perc.Properties.VariableNames{h};
 end
 
-domTab = cell2table(domTab, 'VariableNames', "Dominant band");
-save("dom_waves.mat", "domTab");
+
 
 %% hypnogram
-stages = [{'awake'}, {'stage 1'}, {'stage 2'}, {'stage 3'}, {'stage 4'}];   
+stages = [{'awake'}, {'stage 1'}, {'stage 2'}, {'stage 3'}, {'stage 4'}]; 
+domArr = zeros(length(domTab), 2);
 
+for i = 1:floor(length(EEG)/ep_samples)
+    if domTab{i,1} == "alpha"
+        domArr(i,1) = 1;
+    end
+
+    if domTab{i,2} == "alpha"
+        domArr(i,2) = 1;
+    end
+
+    if domTab{i,1} == "theta"
+           domArr(i,1) = 2;
+    end
+
+    if domTab{i,2} == "theta"
+            domArr(i,2) = 2;
+    end
+
+    if domTab{i,1} == "delta"
+            domArr(i,1) = 3;
+    end
+
+    if domTab{i,2} == "delta"
+            domArr(i,2) = 3;
+    end
+
+end
+
+%alpha 1, theta 2, delta 3
+
+for i = 1:floor(length(EEG)/ep_samples)
+    if i+5 < length(domArr)
+        a = length(find(domArr(i:i+5, 2) == 1)); %alpha count max
+        b = length(find(domArr(i:i+5, 2) == 2)); %theta count max
+        c = length(find(domArr(i:i+5, 2) == 3)); %delta count max
+        d = length(find(domArr(i:i+5, 1) == 1)); %alpha count min
+        e = length(find(domArr(i:i+5, 1) == 3)); %delta count min
+
+        if a >= 3 && e >= 3
+            for j = i:i+5
+                domTab{i,3} = 4; %awake
+            end
+        end
+
+        if b >=3 && a >= c
+            for j = i:i+5
+                domTab{i,3} = 3; %REM
+            end
+        end
+
+        if c >= 3 && d >= 3
+            for j = i:i+5
+                domTab{i,3} = 2; %deep sleep
+            end
+        end
+
+        %         if c > 3
+        %             for j = i: i+5
+        %                 domTab{j, 3} = 2; %deep sleep
+        %             end
+        %         end
+        %
+        %         e = length(find(domArr(i:i+5, 2) == 3));
+        %         if e > 2 && b > 3
+        %             for j = i: i+5
+        %                 domTab{j, 3} = 3; %deep sleep
+        %             end
+    end
+
+    if isempty(domTab{i, 3})
+        domTab{i,3} = 1;
+    end
+
+end
+
+%%
+
+for h = 1:1060
+    hypno(h) = domTab{h,3};
+end
+
+figure
+plot(1:1060, hypno)
+
+
+%%
+
+domTab = cell2table(domTab, 'VariableNames', ["Dominant band", "Stage"]);
+save("dom_waves.mat", "domTab");
