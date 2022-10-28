067b382e12c5b05ee56be2c23a3e7f48825971a2
Giulia Ferri <giulia3.ferri@mail.polimi.it>
2022-10-28 11:51:24 +0200

    Picomaru

--- EEG_sleep.m
+++ EEG_sleep.m
@@ -6,15 +6,32 @@ load("record.mat");
 EEG = double(record);
 clear record
 fs_EEG = 512;
-EEG = EEG(5*60*fs_EEG:end);
-EEG = EEG((length(EEG)/fs_EEG - 5*60)*fs_EEG:-1:1);
+EEG = EEG(2*60*fs_EEG:end);
+EEG = EEG((length(EEG)/fs_EEG - 1*60)*fs_EEG:-1:1);
 N = length(EEG);
+%% Complete Power Spectrum
+noverlap = 0.5*N;
+[Pxx, freq1] = pwelch(EEG, hanning(length(EEG)), noverlap, [], fs_EEG);
+
+figure
+plot(freq1, Pxx)
+xlabel('[Hz]', fontsize=14);
+ylabel('[Power/f]',fontsize=14);
+
 %% Filtering
 %Removing net interference
 [b,a] = iirnotch(60/(fs_EEG/2),0.2/(fs_EEG/2)); %this function wants radians!
-% figure, freqz(b3,a3);
-EEG = filtfilt(b,a, EEG);
+figure 
+%freqz(b,a);
+[hz,hp,hl]=zplane(b,a);
 
+set(hz, markerSize=25, color='b',linewidth=2);
+set(hp, markerSize=25, color='r',linewidth=2);
+set(hl,linewidth=2)
+title('Notch Filter (60 Hz)','FontSize',16)
+
+EEG = filtfilt(b,a, EEG);
+%%
 filter_order = 4;
 
 %Low-pass
@@ -23,23 +40,52 @@ wc = f_cut/(fs_EEG/2);
 [b,a] = butter(filter_order,wc,'low');
 %figure; freqz(b1,a1,1024,fs_EEG);
 
+figure 
+[hz,hp,hl]=zplane(b,a);
+
+set(hz, markerSize=25, color='b',linewidth=2);
+set(hp, markerSize=25, color='r',linewidth=2);
+set(hl,linewidth=2)
+title('Low Pass Filter (70 Hz)','FontSize',16)
+
+
 EEG = filtfilt(b,a,EEG);
 
+%%
 %High-pass
 f_cut = 0.5;
 wc = f_cut/(fs_EEG/2);
 [b,a] = butter(filter_order,wc,'high');
 %figure; freqz(b1,a1,1024,fs_EEG);
+figure 
+[hz,hp,hl]=zplane(b,a);
+
+set(hz, markerSize=25, color='b',linewidth=2);
+set(hp, markerSize=25, color='r',linewidth=2);
+set(hl,linewidth=2)
+title('High Pass Filter (0.5 Hz)','FontSize',16)
+
 
 EEG = filtfilt(b,a,EEG);
 
+%% Plot 
+t= (1:10*fs_EEG)/fs_EEG;
+figure 
+plot(t,EEG(3600*fs_EEG:3610*fs_EEG-1), 'Color', '#123356');
+
+xlabel('Time [s]');
+ylabel('Amplitude [mV]')
+
+%%
 %plotting power spectrum
-noverlap = 20*fs_EEG;
+noverlap = 0.5*N;
 [Pxx, freq1] = pwelch(EEG, hanning(length(EEG)), noverlap, [], fs_EEG);
 
 figure
 plot(freq1, Pxx)
-xlim([0 15])
+xlim([0.5 70])
+
+%% 
 
 %% Dividing into epochs
 % 30 sec
@@ -64,7 +110,7 @@ for i = 1:floor(length(EEG)/ep_samples)
     %PSD with Welch windowing
     noverlap = 20*fs_EEG;
     [Pxx_ep, freq_ep] = pwelch(ep{i}, hanning(ep_samples), noverlap, [], fs_EEG);
-    freq1_ep = 0:fs_EEG/length(Pxx_ep):fs_EEG/2;
+
 
     for j = 1 : length(f_vect_low)
         filter_order = 4;
@@ -97,20 +143,18 @@ var = cell2table(var, 'VariableNames', names);
 perc = cell2table(perc, 'VariableNames', names);
 %save("percentages.mat", "perc");
 
+%% To see the result
+figure
+plot((1:30*fs_EEG)/fs_EEG, ep{689})
+xlim([10,20])
+xlabel('[s]')
+ylabel('[mV]')
 
-% var_per = cell2table(var_per, 'VariableNames', names);
-%save("variances_per.mat", "var_per");
-
-%%
-%Plot of the variances for each frequency band
-% lim = [0 9];
-% figure
-% t = (1:lep:lep*floor(length(EEG)/ep_samples))/3600;
-% subplot(411), plot(t,var.beta), title("Beta"),ylim([0 1]), xlim(lim);
-% subplot(412), plot(t,var.alpha), title("Alpha"),ylim([0 1]), xlim(lim);
-% subplot(413), plot(t,var.theta), title("Theta"),ylim([0 1]), xlim(lim);
-% subplot(414), plot(t,var.delta), title("Delta"),ylim([0 1]), xlim(lim);
-
+figure
+plot((1:30*fs_EEG)/fs_EEG, ep{46})
+xlim([10,20])
+xlabel('[s]')
+ylabel('[mV]')
 %%
 %Plot of the variances for each frequency band
 lim = [0 9];
@@ -120,6 +164,7 @@ t = (1:lep:lep*floor(length(EEG)/ep_samples))/3600;
 plot(t,var.alpha),ylim([-10 50]), xlim(lim);
 hold on
 plot(t,var.theta),ylim([-10 50]), xlim(lim);
+hold on 
 plot(t,var.delta),ylim([-10 50]), xlim(lim);
 
 legend('alpha', 'theta', 'delta')
@@ -185,52 +230,81 @@ for i = 1:floor(length(EEG)/ep_samples)
 
         if a >= 3 && e >= 3
             for j = i:i+5
-                domTab{i,3} = 4; %awake
+                domTab{i,3} = 5; %awake
             end
         end
 
-        if b >=3 && a >= c
+        if b>=3 && a >= c
             for j = i:i+5
-                domTab{i,3} = 3; %REM
+                domTab{i,3} = 4; %REM
             end
         end
 
-        if c >= 3 && d >= 3
+        if b >= 3 && d >= e
             for j = i:i+5
-                domTab{i,3} = 2; %deep sleep
+                domTab{i,3} = 2; %NREM 2 sleep
             end
         end
 
-        %         if c > 3
-        %             for j = i: i+5
-        %                 domTab{j, 3} = 2; %deep sleep
-        %             end
-        %         end
-        %
-        %         e = length(find(domArr(i:i+5, 2) == 3));
-        %         if e > 2 && b > 3
-        %             for j = i: i+5
-        %                 domTab{j, 3} = 3; %deep sleep
-        %             end
+        if c >= 3 && d >= 5
+            for j = i:i+5
+                domTab{i,3} = 1; %NREM 3 deep sleep
+            end
+        end
+        
+
+      
     end
 
-    if isempty(domTab{i, 3})
-        domTab{i,3} = 1;
+    if isempty(domTab{i, 3}) %NREM 1 light sleep
+        domTab{i,3} = 3;
     end
 
 end
 
-%%
+%% Per fare l'hypnogram più leggibile 
+app=cell2mat(domTab(:,3));
+EpTogether = 10;
+for i = 1:EpTogether:length(app)-EpTogether
+    count=zeros(5,1);
+    for k=1:5
+        for j=i:i+EpTogether-1
+            if app(j)==k
+                count(k)=count(k)+1;
+            end
+        end     
+    end
+    [pp,p]=max(count);
+    for j=i:i+EpTogether-1
+        app(j,1)=p;
+    end
+end
 
+%%
+figure
 for h = 1:1060
     hypno(h) = domTab{h,3};
 end
 
-figure
-plot(1:1060, hypno)
+plot(1:1060, hypno, 'LineWidth',0.75,'Color','red')
+
+
+for h = 1:1060
+    hypno(h) = app(h);
+end
+
+hold on
+plot(1:1060, hypno,'LineWidth',3,'Color','Blue')
+hold on
+yline(2.5,'k--','Slow Wave Sleep','LineWidth',1)
+title('Numero di Epoche insieme: ',EpTogether)
+xlabel('Epochs','FontSize',15)
+ylabel('Sleep Stages', 'FontSize',15)
+set(gca,'YTick',0:5);
+yticklabels({'';'NREM3';'NREM2';'NREM1';'REM';'AWAKE'})
+ylim([0,5]);
 
 
 %%
-
-domTab = cell2table(domTab, 'VariableNames', ["Dominant band", "Stage"]);
+domTab = cell2table(domTab, 'VariableNames', ["Less Dominant Band", "Dominant band", "Stage"]);
 save("dom_waves.mat", "domTab");
