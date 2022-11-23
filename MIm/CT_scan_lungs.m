00efd8aa959fac37020ecf9d30c6d087204a00da
Giulia Ferri <giulia3.ferri@mail.polimi.it>
2022-11-23 14:23:12 +0100

    Prove

---
+++ MIm/CT_scan_lungs.m
@@ -0,0 +1,7 @@
+clc
+clear
+close all
+
+%% T0
+t = '0';
+[D, vol,info] = readDCMfolder(t);
--- MIm/readDCMfolder.m
+++ MIm/readDCMfolder.m
@@ -1,19 +1,17 @@
-function [ vol,info ] = readDCMfolder();
-
-% Place the current directory in the folder containing single .dcm files.
-
+function [D, vol,info] = readDCMfolder(t)
 %%
-D = dir('*.dcm');
+folder = ['Patient0/T_', t, '/CT/'];
+D = dir([folder, '*.dcm']);
 
-im = dicomread( D(1).name );
+im = dicomread([folder, D(1).name]);
 vol = zeros(size(im,1), size(im,2), size(D,1));
 
-info = dicominfo( D(1).name );
+info = dicominfo([folder, D(1).name]);
 
 f = waitbar(0, sprintf('Loading:  %u / %u', 0, size(D,1)));
 for ind=1:size(D, 1)
     waitbar(ind/size(D,1), f, sprintf('Loading:  %u / %u', ind, size(D,1)));
-    vol(:,:,ind) = dicomread( D(ind).name );
+    vol(:,:,ind) = dicomread( [folder, D(ind).name] );
 end
 close(f);
 
